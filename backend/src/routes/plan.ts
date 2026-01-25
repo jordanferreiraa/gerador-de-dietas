2171b0d389de0b69bf1731fac57c8b095309f2ab
jordanferreiraa <jordanferreira69@gmail.com>
2026-01-24 21:02:22 -0300

    feat: add routes

---
+++ backend/src/routes/plan.ts
@@ -0,0 +1,9 @@
+import type { FastifyInstance } from "fastify";
+
+export async function planRoutes(app: FastifyInstance) {
+
+    app.post("/plan", async (request, reply) => {
+        return reply.send("Rota de plano de dieta");
+    });
+
+}

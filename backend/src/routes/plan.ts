22af3050d7737de88002bd19b1022a6ee695cd9e
jordanferreiraa <jordanferreira69@gmail.com>
2026-01-28 22:07:02 -0300

    feat: update plan routes

--- backend/src/routes/plan.ts
+++ backend/src/routes/plan.ts
@@ -1,9 +1,25 @@
 import type { FastifyInstance } from "fastify";
+import { DietPlanRequestSchema } from "../types";
+import { generateDietPlan } from "../agent";
 
 export async function planRoutes(app: FastifyInstance) {
-
     app.post("/plan", async (request, reply) => {
-        return reply.send("Rota de plano de dieta");
-    });
+        const parse = DietPlanRequestSchema.safeParse(request.body);
+        if(!parse.success){
+            return reply.status(400).send({
+                error: "ValidationError",
+                details: parse.error.flatten(issue => issue.message),
+            });
+        }
+        
+        try{
+            const data = generateDietPlan(parse.data)
 
+            return reply.send(data);
+        }catch(err: any){
+            request.log.error(err);
+            reply.raw.write(`event: error\n ${JSON.stringify(err.message)}`);
+            reply.raw.end();
+        }
+    });
 }
